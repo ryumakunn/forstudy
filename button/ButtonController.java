2d814d5d526690b4cbb0cb8b801a2a5af89f532a
ryumakunn <154520574+ryumakunn@users.noreply.github.com>
2024-05-31 13:53:43 +0900

    Update ButtonController.java

--- button/ButtonController.java
+++ button/ButtonController.java
@@ -14,29 +14,10 @@ public class ButtonController {
         return "index";
     }
 
-    @PostMapping("/buttonClicked")
-    public String buttonClicked(@RequestParam String button, Model model) {
-        String message;
-        switch (button) {
-            case "Button 1":
-                message = "You clicked Button 1";
-                // Button 1をクリックしたときの処理
-                break;
-            case "Button 2":
-                message = "You clicked Button 2";
-                // Button 2をクリックしたときの処理
-                break;
-            case "Button 3":
-                message = "You clicked Button 3";
-                // Button 3をクリックしたときの処理
-                break;
-            default:
-                message = "Unknown button";
-                // 未知のボタンをクリックしたときの処理
-                break;
-            }
-        model.addAttribute("message", message);
-        return "index";
-    }
+   public String buttonClicked(@RequestParam String button, Model model) {
+     logger.info("Button clicked: " + button);  // ログにメッセージを出力
+    model.addAttribute("button", button);
+    return "index";
+}
 
 }
